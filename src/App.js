1a9b39733f91f0d3416e316bb7476d6471a15888
Jesse Mazzie <jessegalt@yahoo.com>
2019-01-31 09:18:44 -0500

    Added API calls in, working out some bugs still.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import icon from './sun.png';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weather: {}
+    };
+    this.fetchWeather();
+  };
+
+  fetchWeather() {
+    fetch('api.openweathermap.org/data/2.5/weather?zip=26301&APPID=49d7e6b3201708a377f7acfe7d5877e0').then(results => {
+      console.log(results.json());
+      return results.json();
+    }).then(data => {
+      let weatherData = data.results.map((weatherData) => {
+        return(<div>weatherData</div>)
+      })
+      this.setState({weather: weatherData});
+      console.log('HERE' + weatherData);
+    })
+
+  }
+
   render() {
     return (
       <div className="App">
