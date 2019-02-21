2a4bfe4461a607935b0eb47122c2c7d047aa4653
Jesse Mazzie <jessegalt@yahoo.com>
2019-02-21 18:32:07 -0500

    Added property to determine units of measurement.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import icon from './sun.png';
 import './App.css';
 
 class App extends Component {
+  
   constructor() {
     super();
     this.state = {
-      weather: {}
+      weather: {},
+      units: 'imperial'
       //TODO: Add flag in state for switching between imperial/metric
     };
     this.fetchWeather();
   };
 
   fetchWeather() {
-    fetch('http://api.openweathermap.org/data/2.5/weather?id=4802316&units=imperial&APPID=49d7e6b3201708a377f7acfe7d5877e0').then(temp => {
+    fetch('http://api.openweathermap.org/data/2.5/weather?id=4802316&units=' + this.state.units + '&APPID=49d7e6b3201708a377f7acfe7d5877e0').then(temp => {
       return temp.json();
     }).then(data => {
-      this.setState({weather: data});
+      this.setState({weather: data, iconUrl: "http://openweathermap.org/img/w/" + data.weather.icon + ".png"});
       document.title = this.state.weather.main.temp + '°';
-      console.log(this.state.weather.main.temp);
+      console.log(this.state.weather);
     })
 
   }
