e0318979491dc2c21ec8236ed6067b7a5f9e0f4f
Jesse Mazzie <jessegalt@yahoo.com>
2019-02-22 19:25:24 -0500

    Added location retrieval, broke the API call.

--- src/App.js
+++ src/App.js
@@ -3,24 +3,45 @@ import icon from './sun.png';
 import './App.css';
 
 class App extends Component {
-  
+
   constructor() {
     super();
     this.state = {
       weather: {},
-      units: 'imperial'
-      //TODO: Add flag in state for switching between imperial/metric
+      temp: '',
+      units: 'imperial',
+      description: '',
+      lat: '20',
+      lon: '-89'
     };
+    this.getLocation();
     this.fetchWeather();
   };
 
+  getLocation() {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        // console.log(position.coords.longitude);
+        // console.log(position.coords.latitude);
+        // this.setState({lon: Math.trunc(position.coords.longitude), 
+        //                lat: Math.trunc(position.coords.latitude)});
+        console.log(this.state.lat);
+      });
+    }
+  }
+
   fetchWeather() {
-    fetch('http://api.openweathermap.org/data/2.5/weather?id=4802316&units=' + this.state.units + '&APPID=49d7e6b3201708a377f7acfe7d5877e0').then(temp => {
+    console.log(this.state);
+    fetch('http://api.openweathermap.org/data/2.5/weather?lat= ' + this.state.lat + '&lon=' + this.state.lon + '&units=' + this.state.units + '&APPID=49d7e6b3201708a377f7acfe7d5877e0').then(temp => {
       return temp.json();
     }).then(data => {
-      this.setState({weather: data, iconUrl: "http://openweathermap.org/img/w/" + data.weather.icon + ".png"});
-      document.title = this.state.weather.main.temp + '°';
-      console.log(this.state.weather);
+      //console.log(data);
+      //this.setState({ weather: data, temp: data.main.temp, 
+      //                description: data.weather[0].description })
+      
+      //document.title = this.state.weather.main.temp + '°';
+      
+      console.log(this.state)
     })
 
   }
@@ -32,7 +53,7 @@ class App extends Component {
           <title></title>
         </header>
         <canvas>
-          
+
         </canvas>
       </div>
     );
